d5628fe025bd4f76746c6f55127c86935be54571
Kavidanimesha <kavindanimesha@gmail.com>
2024-01-23 23:54:54 +0530

    Delete Backend

--- Helper.jsx
+++ Helper.jsx
@@ -385,3 +385,16 @@ const StockEnter = () => {
 }
 
 export default StockEnter
+
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express() 
+
+app.use(express.json())
+app.use(cors())
+
+app.listen (3001, () => {
+    console.log("Server is Running")
+})
